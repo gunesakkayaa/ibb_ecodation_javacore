5a3eea17a10338a99c0b827aee5cb89d6b9de118
Güneş Akkaya <gunesakkaya02@gmail.com>
2025-02-06 18:31:40 +0300

    shorcuts + readme

--- src/main/java/com/gunesakkaya/_1_part_javatypes/_01_JavaTypes.java
+++ src/main/java/com/gunesakkaya/_1_part_javatypes/_01_JavaTypes.java
@@ -1,4 +1,15 @@
 package com.gunesakkaya._1_part_javatypes;
 
+// single comment
+/* multiple comment */
 public class _01_JavaTypes {
+
+    // psvm
+    public static void main(String[] args) {
+        // Çıktı
+        //sout => tab
+        System.out.println();
+    }
+
+
 }
