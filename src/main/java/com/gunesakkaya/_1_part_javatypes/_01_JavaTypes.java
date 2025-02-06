d0a5bd39b6acb1a2856b3e05674a19cf9ce12882
Güneş Akkaya <gunesakkaya02@gmail.com>
2025-02-06 18:37:01 +0300

    news

--- src/main/java/com/gunesakkaya/_1_part_javatypes/_01_JavaTypes.java
+++ src/main/java/com/gunesakkaya/_1_part_javatypes/_01_JavaTypes.java
@@ -1,15 +1,11 @@
 package com.gunesakkaya._1_part_javatypes;
-
 // single comment
 /* multiple comment */
 public class _01_JavaTypes {
-
-    // psvm
+    // psvm => tab
     public static void main(String[] args) {
         // Çıktı
         //sout => tab
-        System.out.println();
-    }
-
-
-}
+        System.out.println("Mrs Gunes Akkaya");
+    } //end psvm
+} //end class
