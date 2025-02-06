0c3001f52880c742c3080262e5296e7959869cec
Güneş Akkaya <gunesakkaya02@gmail.com>
2025-02-06 19:08:24 +0300

    Java Primitive Types class

--- src/main/java/com/gunesakkaya/_1_part_javatypes/_01_JavaPsvm.java
+++ src/main/java/com/gunesakkaya/_1_part_javatypes/_01_JavaPsvm.java
@@ -1,7 +1,7 @@
 package com.gunesakkaya._1_part_javatypes;
 // single comment
 /* multiple comment */
-public class _01_JavaTypes {
+public class _01_JavaPsvm {
     // psvm => tab
     public static void main(String[] args) {
         // Çıktı
