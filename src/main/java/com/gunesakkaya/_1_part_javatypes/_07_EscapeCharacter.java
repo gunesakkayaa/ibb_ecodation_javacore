4db17604bceb827311bf98f9ed3585e7b064c9ed
Güneş Akkaya <gunesakkaya02@gmail.com>
2025-02-07 22:25:44 +0300

    Escape Character

--- src/main/java/com/gunesakkaya/_1_part_javatypes/_07_EscapeCharacter.java
+++ src/main/java/com/gunesakkaya/_1_part_javatypes/_07_EscapeCharacter.java
@@ -3,6 +3,11 @@ package com.gunesakkaya._1_part_javatypes;
 public class _07_EscapeCharacter {
 
     public static void main(String[] args) {
-
+        System.out.println("Gunes, Akkaya!"); // Normal metin
+        System.out.println("Gunes,\nAkkaya!"); // Yeni satır (\n)
+        System.out.println("Gunes,\tAkkaya!"); // Tab boşluğu (\t)
+        System.out.println("Gunesin \"Java\" dili!"); // Çift tırnak (\")
+        System.out.println("Onun\'Agatha kitapları!"); // Tek tırnak (\')
+        System.out.println("C:\\Users\\Gunes\\Desktop"); // Ters eğik çizgi (\\)
     }
 }
--- src/main/java/com/gunesakkaya/_1_part_javatypes/_08_Scanner.java
+++ src/main/java/com/gunesakkaya/_1_part_javatypes/_08_Scanner.java
@@ -5,4 +5,5 @@ public class _08_Scanner {
     public static void main(String[] args) {
 
     }
+
 }
