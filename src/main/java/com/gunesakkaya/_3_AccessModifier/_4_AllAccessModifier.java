fdce9553dcec25882851e1037beb9d63588b0147
Güneş Akkaya <gunesakkaya02@gmail.com>
2025-02-25 18:26:23 +0300

    JAVA | Updates

---
+++ src/main/java/com/gunesakkaya/_3_AccessModifier/_4_AllAccessModifier.java
@@ -0,0 +1,15 @@
+package com.gunesakkaya._3_AccessModifier;
+
+import com.gunesakkaya._3_week.Week3_02_Access_1_PublicAccessModifier;
+import com.gunesakkaya.utils.SpecialColor;
+
+public class _4_AllAccessModifier {
+
+    public static void main(String[] args) {
+        Week3_02_Access_1_PublicAccessModifier accessModifier= new Week3_02_Access_1_PublicAccessModifier();
+        System.out.println(SpecialColor.BLUE+accessModifier.publicData+SpecialColor.RESET);
+        //System.out.println(SpecialColor.YELLOW+accessModifier.defaultData+SpecialColor.RESET);
+        //System.out.println(SpecialColor.PURPLE+accessModifier.protectedData+SpecialColor.RESET);
+        //System.out.println(SpecialColor.RED+accessModifier.privateData+SpecialColor.RESET);
+    }
+}
---
+++ src/main/java/com/gunesakkaya/_3_week/Week3_07_Class_Examples_StudentInformationSystem.java
@@ -0,0 +1,5 @@
+package com.gunesakkaya._3_week;
+
+public class Week3_07_Class_Examples_StudentInformationSystem {
+
+}
